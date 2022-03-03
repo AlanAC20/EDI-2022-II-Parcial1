e5e4b1ac1b6f29d93b0474d5ccc3b36dc6a85a36
Alan Cárcamo <a326769@alumnos.uaslp.mx>
2022-03-02 23:01:35 -0600

    TAREA ARREGLOS

--- T7_P1_E1.c
+++ T7_P1_E1.c
@@ -5,19 +5,46 @@
 #include <stdio.h>
 #define MAX 100
 
+void pideCadena(char cadena[MAX]);
+void pideCaracter(char *caracter);
+void numeroVeces(char cadena[MAX], char caracter, int *contador);
+
+
 int main()
 {
     char cadena[MAX];
+    char caracter;
+    int contador;
 
-    printf("Pon la cadena: ");
-    scanf("%[^\n]", cadena);
-
-    printf("La cadena es: %s", cadena);
-
-
-
-
+    pideCadena(&cadena[MAX]);
+    pideCaracter(&caracter);
+    numeroVeces(&cadena[MAX], caracter, &contador);
+    printf("El caracter aparece %d", contador);
+    printf(" veces");
 
     return 0;
 
+}
+
+
+void pideCadena(char cadena[MAX])
+{
+    printf("Cual es tu cadena de caracteres: ");
+    scanf("%[^\n]", cadena);
+}
+
+
+void pideCaracter(char *caracter)
+{
+    printf("\nQue caracter quieres encontrar: ");
+    scanf("%s", caracter);
+}
+
+
+
+void numeroVeces(char cadena[MAX], char caracter, int *contador)
+{
+    for(int i=0; i <= MAX -1 ; i++)
+        if(cadena[i] == caracter)
+            *contador = *contador +1;
 }

403b03f2f6728d26f28cff05e7d89a463f9ddce4
Alan Cárcamo <a326769@alumnos.uaslp.mx>
2022-03-06 22:55:59 -0600

    TAREA 7

--- T7_P1_E2.c
+++ T7_P1_E2.c
@@ -5,23 +5,62 @@
 #include <stdio.h>
 
 #define MAX 5
+#define LIMITE 100
 
-int main()
-{
+
+void sumaPosiciones(int arreglo[MAX]);
+void sumaPares(int arreglo[MAX]);
+void sumaTotal(int arreglo[MAX]);
+
+int main() {
     int arreglo[MAX];
     int i;
-    int numero;
-    int suma;
 
-    for(int i=0; i <= MAX -1 ; i++){
-    printf("Dame numeros del 1 al 100:");
-    scanf("%d", &arreglo[MAX]);
+
+    for (i = 0; i < MAX; i++) {
+        printf("Dame numeros del 1 al %d:\n", LIMITE);
+        scanf("%d", &arreglo[i]);
     }
 
-    for(int i=0; i <= MAX -1 ; i++) {
-        if(arreglo[MAX] % 2 == 0)
-            suma = arreglo[MAX] + arreglo[MAX];
+    sumaPosiciones(arreglo);
+    sumaPares(arreglo);
+    sumaTotal(arreglo);
+
+}
+
+
+void sumaPosiciones( int arreglo[MAX]){
+    int total, i;
+    for (i = 0; i < MAX; i += 2) {
+        total = total + arreglo[i];
     }
 
-    printf("La suma del arreglo es: %d", suma);
+    printf("suma de las posiciones pares es: %d\n", total);
+}
+
+void sumaPares(int arreglo[MAX]){
+    int i;
+    int suma=0;
+    for (i = 0; i < MAX; i++) {
+        if (arreglo[i] % 2 == 0)
+            suma = suma + arreglo[i];
+        else
+            suma = suma + 0;
+    }
+
+    printf("La suma de los valores pares es: %d\n", suma);
+}
+
+void sumaTotal(int arreglo[MAX]){
+    int i;
+    int sumaTodo= 0;
+    int elementosSumados;
+    for (i = 0; i < MAX; i++) {
+        if (sumaTodo + arreglo[i] <= LIMITE){
+            sumaTodo = sumaTodo + arreglo[i];
+            elementosSumados = i +1;
+        }
+    }
+    printf("La suma total de los valores es: %d\n", sumaTodo);
+    printf("los elementos sumados fueron: %d", elementosSumados);
 }
---
+++ T7_P1_E3.c
@@ -0,0 +1,76 @@
+//
+// Created by carca on 06/03/2022.
+//
+
+#include <stdio.h>
+#define PRODUCTO 4
+
+
+int main ()
+{
+
+    int clave[PRODUCTO];
+    float precio[PRODUCTO];
+    int stock[PRODUCTO];
+    int mayor = 0;
+    float total=0;
+    int menorStock = 0;
+    int i;
+
+    for(i = 0; i < PRODUCTO; i++){
+        printf("Cual es la clave del producto %d de %d:\n", i+1 ,PRODUCTO);
+        scanf("%d", &clave[i]);
+    }
+
+    for(i = 0; i < PRODUCTO; i++){
+        printf("Cual es el precio del producto %d\n", clave[i]);
+        scanf("%f", &precio[i]);
+    }
+
+    for(i = 0; i < PRODUCTO; i++){
+        printf("Cual es el stock del producto %d\n", clave[i]);
+        scanf("%d", &stock[i]);
+
+    }
+
+    for(i = 0; i < PRODUCTO; i++){
+        if(i == 0)
+            mayor = precio[i];
+
+        if(precio[i] > mayor)
+            mayor = precio[i];
+    }
+    for(i = 0; i < PRODUCTO; i++)
+    printf("El producto con mayor precio es: %d\n", mayor);
+
+
+    for(i = 0; i < PRODUCTO; i++){
+        if(i == 0)
+            menorStock = stock[i];
+
+        if(stock[i] < menorStock)
+            menorStock = stock[i];
+    }
+
+    printf("El producto con menor stock es: %d\n", menorStock);
+
+
+
+
+
+
+
+
+    for(i = 0; i < PRODUCTO; i++){
+        total = total + (precio[i] * stock[i]);
+
+    }
+
+    printf("El total de todo el stock es: %f\n", total);
+
+
+
+
+
+
+}
